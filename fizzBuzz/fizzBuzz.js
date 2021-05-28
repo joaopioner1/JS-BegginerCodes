8cb3fef988c72cbf4d3006260372d7552a113704
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-05-28 12:00:57 -0300

    Finished FizzBuzz

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,13 +1,13 @@
 function fizzBuzz(input) {
-   if (input % 3 === 0) {
-      return "fizz";
-   } 
-   else if (input % 2 === 0) {
-      return "buzz";
-   } else {
-      return "It's not a number!";
-   }
+  if (input % 3 === 0 && input % 5 === 0) 
+    return "FizzBuzz"
+  if (input % 3 === 0) 
+    return "fizz";
+  if (input % 2 === 0) 
+    return "buzz";
+  else 
+    return "It's not a number!";
 }
 
-const result = fizzBuzz(true);
+const result = fizzBuzz(15);
 console.log(result);
