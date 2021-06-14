8cbabf9017e6b53db298235eefe91a56b8f0b0f4
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-06-14 14:03:20 -0300

    primo ex not working

---
+++ primos/script.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+function exibirNumPrimos(limite) {
+  for (let i = 1; i <= limite; i++) {
+    for (let j = 1; j <= 10; j++) {
+      if (i % j == 0) {
+        count += 1;
+        if (count > 1) {
+          return i;
+        }
+      }
+    }
+  }
+}
+
+console.log(exibirNumPrimos(15));
